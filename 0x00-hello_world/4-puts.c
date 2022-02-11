9de3f8ea6af342a9ed04f0f33b499666ff7931a1
Ivan Rosero <ivan.rosero@outlook.com>
2022-02-11 07:15:00 -0800

    Hello, put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
-/** main - This program prints the sentence specified using the function puts */
+/**
+ * main - prints the phrase
+ * Return: zero
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
