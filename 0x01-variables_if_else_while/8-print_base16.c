798ab3040e1549bf5cd357df6132246c195e079e
Ivan Rosero <ivan.rosero@outlook.com>
2022-02-11 15:48:07 -0800

    Base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - This program prints the numbers 0 to 9
+ * and then the letters a to f
+ * Return: zero
+ */
+
+int main(void)
+{
+	int sxtn;
+
+	for (sxtn = 48; sxtn < 58; sxtn++)
+	{
+		putchar(sxtn);
+	}
+	for (sxtn = 97; sxtn <= 102; sxtn++)
+	{
+		putchar(sxtn);
+	}
+	putchar('\n')
+	return (0);
+}
