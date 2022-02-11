41f0e3ee7b274c7ad8e8bd6c3620da687a857c35
Ivan Rosero <ivan.rosero@outlook.com>
2022-02-11 11:45:10 -0800

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - This program prints the alphabet in lowercase
+ *
+ * Return: zero
+ */
+
+int main(void)
+{
+	char alf;
+	alf = abcdefghijklmnopqrstuvwxyz;
+	do{
+		putchar(alf);
+	}
+return (0);
+}
