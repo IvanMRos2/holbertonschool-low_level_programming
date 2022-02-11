5ed3ae367f026b254c8e004a9f22488a86d5fe56
Ivan Rosero <ivan.rosero@outlook.com>
2022-02-11 14:07:19 -0800

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - This program prints the numbers only if
+ * they are base of 10
+ * Return: zero
+ */
+
+int main(void)
+{
+	int ten;
+
+	for (ten = 0; ten < 10; ten++)
+	{	printf("%d", ten);
+	}
+	printf("\n");
+	return (0);
+}
