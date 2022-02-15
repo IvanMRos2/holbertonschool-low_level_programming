f5b0f0f7d9db898d66312e28bfd969c30cb0a787
Ivan Rosero <ivan.rosero@outlook.com>
2022-02-15 14:28:41 -0800

    Add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - It makes the addition of two numbers
+ * @p: It's one number
+ * @q: It's one number
+ * Return: prints r
+ */
+
+int add(int p, int q)
+{
+	int r;
+
+	r = p + q;
+	return (r);
+}
