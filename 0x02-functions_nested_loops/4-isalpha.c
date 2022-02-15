df249158eaae93828315905540412cf1dedca5a0
Ivan Rosero <ivan.rosero@outlook.com>
2022-02-15 05:49:13 -0800

    Isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,16 +1,16 @@
-#include "main.h"                                                 
-                                                                 
-/**                                                               
- * main - Checks lowercase character
- *                                               
- * Return: 1 if c is lowercase or uppercase 0 if not      
- */                                                               
-int _isalpha(int c)                                               
+#include "main.h"
+/**
+ * _isalpha - Checks the lowercase characters
+ * @c: The character that is variable
+ * Return: if c is lowercase or uppercase 1, else 0
+ */
+int _isalpha(int c)
 
-{                                                               
-       if (((c >= 'a' && c <= 'z')) || ((c >= 'A') && (c <= 'Z')))                                 
-                return(1);                                        
-        else                                                      
-		return(0);                                        
+{
+	if (((c >= 'a') && (c <= 'z'))
+		|| ((c >= 'A') && (c <= 'Z')))
 
+	return (1);
+	else
+	return (0);
 }
