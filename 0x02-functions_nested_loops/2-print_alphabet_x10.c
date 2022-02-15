0dc6a799244591829af869008a6a7079ae3c4391
Ivan Rosero <ivan.rosero@outlook.com>
2022-02-15 07:34:41 -0800

    Alphabetx10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,20 +1,21 @@
 #include "main.h"
+
 /**
  * print_alphabet_x10 - Prints 10 times the alphabet
- * Return: No return
+ * Return : No return
  */
 
 void print_alphabet_x10(void)
 {
-	int alf = 0;
-	char ten;
+	char alf;
+	int ten = 0;
 
-	while (alf < 10)
+	while (ten++ < 10)
 	{
-		for (ten = 'a'; ten <= 'z'; ten++)
+		for (alf = 'a'; alf <= 'z'; alf++)
 		{
-			_putchar(ten);
+			_putchar(alf);
 		}
 		_putchar('\n');
-	}	alf++;
+	}
 }
