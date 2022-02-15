dbdaa26182aa1b752f6d818e71182c83c6eecf57
Ivan Rosero <ivan.rosero@outlook.com>
2022-02-15 06:27:27 -0800

    Absolut

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,13 +2,14 @@
 
 /**
  * _abs -This project prints the absolute value of integer numbers
+ * @I: The variable for the integer number
  * Return: The absolute value of the integer number
  */
 
 int _abs(int I)
 {
 	if (I >= 0)
-	{ 
+	{
 		return (I);
 	}
 	else
