46469583e960114f76fee0c90e7275ea616d6bba
Ivan Rosero <ivan.rosero@outlook.com>
2022-02-17 07:18:04 -0800

    Supper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isupper -This program prints the alphabet in Uppercase
+ * @c: character to check if it's uppercase
+ * Return: 1 if the character is Uppercase and 0 if else.
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	return (0);
+}
