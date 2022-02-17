0fce4334f061fdd097e71f40a6924e0090a113c0
Ivan Rosero <ivan.rosero@outlook.com>
2022-02-17 12:34:32 -0800

    More Numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * more_numbers - This function prints 10 times the numbers from 0 to 14
+ */
+
+void more_numbers(void)
+{
+	int line;
+	char column;
+	
+	for (line = 0; line <= 9; line++)
+	{
+		for (column = 0; column <= 14 ; column++)
+		{
+			if (column > 9)
+			{
+				_putchar ((column / 10) + '0');
+			}
+			_putchar ((column % 10) + '0');
+		}
+			_putchar ('\n');
+	}
+}
