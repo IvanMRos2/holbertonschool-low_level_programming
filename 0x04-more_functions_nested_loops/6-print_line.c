e48b1dc8b375ae51281703e4612dd189f70c8521
Ivan Rosero <ivan.rosero@outlook.com>
2022-02-17 13:03:50 -0800

    Lines

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,13 +7,10 @@
 
 void print_line(int n)
 {
-	int line;
-
-	if (n > 0)
+	while (n > 0)
 	{
-	for (line = 0; line <= n; line++)
-
-	_putchar ('_');
+		_putchar ('_');
+		n--;
 	}
 	_putchar ('\n');
 }
