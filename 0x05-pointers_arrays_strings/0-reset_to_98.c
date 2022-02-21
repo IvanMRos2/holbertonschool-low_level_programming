785d3265df2af8cb5c97a76f2c5be329f5a60bc2
IvanMRos2 <ivan.rosero@outlook.com>
2022-02-21 13:09:19 -0500

    Reset

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,10 +3,12 @@
 /**
  * reset_to_98 - This function updates the value it points to 98
  * @n: The variable that will point to the other value
+ * @o: The New variable
  * Return: No return
  */
 
 void reset_to_98(int *n)
 {
-	*n = 98;
+	int o = 98
+	*n = o;
 }
