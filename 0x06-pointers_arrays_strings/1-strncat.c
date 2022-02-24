f5126b8888f51f2ea07709fecc80faee627f8ffd
IvanMRos2 <ivan.rosero@outlook.com>
2022-02-24 09:02:45 -0500

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,15 +11,18 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int hello = 0, world = 0, hw = 0;
+	int hw = 0, w = 0;
 
-	while (dest[hello] != '\0')
+	while (dest[hw] != '\0')
 	{
-		hello++;
+		hw++;
 	}
-		while ((dest[hello + world] = *src++) != '\0' && hw < n - 1)
-	{	hw++;
-		world++;
+	while (src[w] != '\0' && w < n)
+	{	
+		dest[hw] = src[w];
+		hw++;
+		w++;
 	}
+	
 	return (dest);
 }
