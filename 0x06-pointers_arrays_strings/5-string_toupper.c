4833cc7fb1689e8e6f8ff05369542da9f9538e96
IvanMRos2 <ivan.rosero@outlook.com>
2022-02-24 19:52:06 -0500

    Upper

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -2,26 +2,20 @@
 
 /**
  * string_toupper - Changes lowercase into uppercase
- * 
+ * @str: String to be printed
  * Return: string
  */
 
 char *string_toupper(char *str)
 {
 	int lw;
-	int up;
-	char lwr[] = {97 .. 122};
-	char upr[] = {65 .. 90};
 
 	for (lw = 0; str[lw] != '\0'; lw++)
 	{
-		for (up = 0; lwr[up] != '\0'; up++)
-		{
-		if (str[lw] == lwr[up])
-		{
-			str[lw] = upr[nm];
-		}
-		}
-	{	
+		if (str[lw] >= 97 && str[lw] <= 122)
+	{
+		str[lw] = str[lw] - 32;
+	}
+	}
 	return (str);
 }
