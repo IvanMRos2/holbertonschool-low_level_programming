737cf7b63bc146b0cfd63437ae06dfcbecffaae4
IvanMRos2 <ivan.rosero@outlook.com>
2022-03-01 10:56:04 -0500

    chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- *
- *
- *
+ * print_chessboard - This function prints the chessboard
+ * @a: The string to be printed as columns and lines
+ * Return: No return
  */
 
 void print_chessboard(char (*a)[8])
-{	
+{
 	int col, fil;
 
 	for (col = 0; col < 8; col++)
@@ -15,8 +15,7 @@ void print_chessboard(char (*a)[8])
 		for (fil = 0; fil < 8; fil++)
 		{
 		_putchar(a[col][fil]);
-	
 		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
