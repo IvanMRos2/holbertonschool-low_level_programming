b565b0be1638f7a5f19c2a9a28b1558b9d4f572d
IvanMRos2 <ivan.rosero@outlook.com>
2022-02-28 20:20:09 -0500

    return

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -12,11 +12,11 @@ char *_strchr(char *s, char c)
 {
 	int ctr = 0;
 
-	while (s[ctr] >= '\0')
+	while (s[ctr] != '\0')
 	{
 		ctr++;
 		if (s[ctr] == c)
-			return (s + ctr);
+			return (&s[ctr]);
 	}
 
 	return ('\0');
