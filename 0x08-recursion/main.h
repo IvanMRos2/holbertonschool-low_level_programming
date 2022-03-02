ba0d3dec58493514acefc32c98e65af4efa254ec
IvanMRos2 <ivan.rosero@outlook.com>
2022-03-02 18:05:45 -0500

    sqrt

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 
 int _putchar(char c);
+int scnd_sqrt_recursion(int n, int q);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
