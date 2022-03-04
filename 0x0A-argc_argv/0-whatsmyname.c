578d797bf52112029468a287a774d58aa7884f2c
IvanMRos2 <ivan.rosero@outlook.com>
2022-03-04 16:08:44 -0500

    rawr or something

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - This function prints the name of the program
+ * @argc: Number of arguments supplied to the program
+ * @argv: Array of pointers to the arguments
+ * Return: 0
+ */
+int main(__attribute__((unused))
+int argc, char *argv[])
+
+{
+	{
+		printf("%s\n", *argv);
+	}
+
+return (0);
+
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+# Functions argc, argv
